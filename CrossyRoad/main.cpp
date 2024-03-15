6d2db724e4ff3a2b66ab22e59adf90e8dfd72d06
Nguyễn Xuân Thịnh <161504321+thinh2711@users.noreply.github.com>
2024-03-15 21:43:17 +0700

    Add files via upload

--- CrossyRoad/main.cpp
+++ CrossyRoad/main.cpp
@@ -1,1 +1,11 @@
-#include<iostream>
+#include "Genio.h"
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+
+using namespace std;
+
+enum TileType { GRASS, WATER, ROAD, RAIL, TREE };
+enum ObjectType { PLAYER, CAR, TRAIN, STICK, LAMP, EAGLE, COIN };
+enum GameState { START, PLAY, EXITE, PAUSE, GAME_OVER, OUT, CHOOSE_PLAYER };
+enum Direction { UP, DOWN, LEFT, RIGHT };
