ef2fe77718e86bc5c5ac7a1f245a43f50b9c501c
Nguyễn Xuân Thịnh <161504321+thinh2711@users.noreply.github.com>
2024-03-19 21:24:29 +0700

    Add files via upload

---
+++ CrossyRoad/GameLogic.h
@@ -0,0 +1,13 @@
+// GameLogic.h
+#ifndef GAME_LOGIC_H
+#define GAME_LOGIC_H
+
+void update();
+void checkPlayerStatus();
+void adjustCameraSpeed();
+void generateTiles(int);
+void addObjects(int);
+void addCar(int);
+void addStick(int);
+
+#endif // GAME_LOGIC_H
---
+++ CrossyRoad/Rendering.h
@@ -0,0 +1,8 @@
+// Rendering.h
+#ifndef RENDERING_H
+#define RENDERING_H
+
+void drawTiles();
+void drawObjects();
+
+#endif // RENDERING_H
