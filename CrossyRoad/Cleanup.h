5564a7015b40b950304fbe673242610d83a55af5
thinh2711 <thinhcuc6@gmail.com>
2024-03-27 19:03:16 +0800

    CLeanup

--- CrossyRoad/Cleanup.h
+++
@@ -1,7 +0,0 @@
-// ClEANUP_H
-#ifndef CLEANUP_H
-#define CLEANUP_H
-
-void destroy();
-
-#endif // CLEANUP_H
--- CrossyRoad/Rendering.h
+++
@@ -1,8 +0,0 @@
-// Rendering.h
-#ifndef RENDERING_H
-#define RENDERING_H
-
-void drawTiles();
-void drawObjects();
-
-#endif // RENDERING_H
