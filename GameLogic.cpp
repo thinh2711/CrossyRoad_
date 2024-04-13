3c41c3e437561aef92b37c81800434c010c2e201
Nguyễn Xuân Thịnh <161504321+thinh2711@users.noreply.github.com>
2024-04-13 22:45:52 +0800

    Add game func handle guess event

--- GameLogic.cpp
+++ GameLogic.cpp
@@ -13,3 +13,33 @@ HangmanGame::HangmanGame(WINDOWS* WINDOWS, int time) : SDL(WINDOWS), playTime(ti
     countwin = 0;
     countloss = 0;
 }
+void HangmanGame::initWord() {
+    word = chooseWord(fileName, diff);
+    if (word.empty()) {
+        cout << "No word available in " << fileName << endl;
+        gameplay = false;
+        quit = true;
+    }
+}
+
+void HangmanGame::guessEvent() {
+    SDL_Event event;
+    guessChar = ' ';
+    if (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            gameplay = false;
+            quit = true;
+        } else if (event.type == SDL_KEYUP && event.key.keysym.sym == SDLK_ESCAPE) {
+            gameplay = false;
+        } else if (event.type == SDL_KEYUP) {
+            string keyName = SDL_GetKeyName(event.key.keysym.sym);
+            if (keyName == "Escape")
+                gameplay = false;
+            else if (keyName == "Space")
+                guessChar = '$';
+            else if (keyName.length() == 1 && keyName[0] >= 'A' && keyName[0] <= 'Z')
+                guessChar = keyName[0];
+        }
+    }
+}
+
