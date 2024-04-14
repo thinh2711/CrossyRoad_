e69c52617b599019ced81b854c139088fef9c4bb
Nguyễn Xuân Thịnh <161504321+thinh2711@users.noreply.github.com>
2024-04-14 17:20:53 +0800

    Fix color and add moving

--- draw.cpp
+++ draw.cpp
@@ -1,7 +1,7 @@
 #include "draw.h"
 
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_ttf.h>
+#include "SDL2/SDL_image.h"
+#include "SDL2/SDL_ttf.h"
 
 #include <cmath>
 #include <cstdlib>
@@ -9,12 +9,12 @@
 
 using namespace std;
 
-void DRAW::setPosition(float x, float y) {
+void DRAW::setPosition(double x, double y) {
     this->x = x;
     this->y = y;
 }
 
-void DRAW::setAngle(float angle) {
+void DRAW::setAngle(double angle) {
     this->angle = angle - floor(angle / 360) * 360;
 }
 
@@ -24,39 +24,40 @@ void DRAW::setColor(SDL_Color color) {
 }
 
 void DRAW::clearWithBgColor(SDL_Color bgColor) {
-    SDL_Color curColor = color;
+    SDL_Color Color_ = color;
     setColor(bgColor);
     SDL_RenderClear(renderer);
-    setColor(curColor);
+    setColor(Color_);
 }
 
-SDL_Texture* DRAW::loadTexture(string path) {
+SDL_Texture* DRAW::loadTexture(string filepath) {
     SDL_Texture* newTexture = NULL;
-    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    SDL_Surface* loadedSurface = IMG_Load(filepath.c_str());
     if (loadedSurface == NULL)
-        cout << "Unable to load image " << path << " SDL_image Error: " << IMG_GetError() << endl;
+        cout << "Unable to load image " << filepath << " SDL_image Error: " << IMG_GetError() << endl;
     else {
         newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
         if (newTexture == NULL)
-            cout << "Unable to create texture from " << path << " SDL Error: " << SDL_GetError() << endl;
+            cout << "Unable to create texture from " << filepath << " SDL Error: " << SDL_GetError() << endl;
         SDL_FreeSurface(loadedSurface);
     }
     return newTexture;
 }
 
-SDL_Texture* DRAW::loadImage(string path, SDL_Rect* srcRest, SDL_Rect* desRect, float _x, float _y) {
+SDL_Texture* DRAW::loadImage(string filepath, SDL_Rect* srcRest, SDL_Rect* desRect, double _x, double _y) {
     SDL_Texture* newTexture = NULL;
-    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    SDL_Surface* loadedSurface = IMG_Load(filepath.c_str());
     if (loadedSurface == NULL)
-        cout << "Unable to load image " << path << " SDL_image Error: " << IMG_GetError() << endl;
+        cout << "Unable to load image " << filepath << " SDL_image Error: " << IMG_GetError() << endl;
     else {
         newTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
         if (newTexture == NULL)
-            cout << "Unable to create texture from " << path << " SDL Error: " << SDL_GetError() << endl;
+            cout << "Unable to create texture from " << filepath << " SDL Error: " << SDL_GetError() << endl;
         SDL_FreeSurface(loadedSurface);
     }
 
-    SDL_QueryTexture(newTexture, NULL, NULL, &srcRest->w, &srcRest->h);
+    Uint32 format;
+    SDL_QueryTexture(newTexture, &format, NULL, &srcRest->w, &srcRest->h);
     srcRest->x = 0;
     srcRest->y = 0;
     desRect->x = _x;
@@ -66,7 +67,7 @@ SDL_Texture* DRAW::loadImage(string path, SDL_Rect* srcRest, SDL_Rect* desRect, 
     return newTexture;
 }
 
-SDL_Texture* DRAW::textTexture(string text, SDL_Rect* srcRest, SDL_Rect* desRect, float _x, float _y) {
+SDL_Texture* DRAW::textTexture(string text, SDL_Rect* srcRest, SDL_Rect* desRect, double _x, double _y) {
     SDL_Color a = {0, 0, 0};
     SDL_Surface* surface = TTF_RenderText_Blended(font, text.c_str(), a);
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
@@ -92,9 +93,29 @@ DRAW::DRAW(SDL_Window* window = nullptr, SDL_Renderer* renderer_ = nullptr, TTF_
     SDL_RenderGetLogicalSize(renderer, &width, &height);
     if (width == 0 && height == 0)
         SDL_GetWindowSize(window, &width, &height);
-
+    
     setPosition(width / 2, height / 2);
     setAngle(0);
     setColor(WHITE_COLOR);
     clearWithBgColor(WHITE_COLOR);
+}
+
+void DRAW::getRandomColor() {
+    Uint8 r = rand() % 256;
+    Uint8 g = rand() % 256;
+    Uint8 b = rand() % 256;
+    SDL_Color color = {r, g, b};
+    setColor(color);
+}
+
+void DRAW::moveForward(double length) {
+    double prevX = x, prevY = y;
+    jumpForward(length);
+    SDL_RenderDrawLine(renderer, (int)prevX, (int)prevY, (int)x, (int)y);
+}
+
+void DRAW::jumpForward(double length) {
+    double rad = (angle / 180) * M_PI;
+    x += (cos(rad) * length);
+    y -= (sin(rad) * length);
 }
